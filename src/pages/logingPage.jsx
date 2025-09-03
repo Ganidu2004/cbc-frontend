809cb67aacaf9df7a0beb3d0b078d512005093b9
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-09-03 16:32:04 +0530

    delete css file💥

--- src/pages/logingPage.jsx
+++ src/pages/logingPage.jsx
@@ -2,10 +2,7 @@ import "./logingPages.css";
 
 export default function LoginPage() {
   return (
-    <div className="login-container">
-      <h1>Login Page</h1>
-      <input type="text" placeholder="Enter your username" />
-      <input type="password" placeholder="Enter your password" />
+    <div>
       <button>Login</button>
     </div>
   );
