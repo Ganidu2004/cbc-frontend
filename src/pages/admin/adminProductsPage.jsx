de134216c7c410b0b15843ffc2b8d3cbae7cd9a5
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-09-08 19:01:07 +0530

    useEffect hook used😊

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,14 +1,71 @@
 import axios from "axios"
+import { useEffect, useState } from "react"
 
 export default function AdminProduct(){
 
-    axios.get("http://localhost:5000/api/product").then((res)=>{
-        console.log(res)
-    })
+    const [products,setProducts] = useState([
+    {
+        "_id": "68b3078362262df9b7fd07ed",
+        "productId": "B2001",
+        "productName": "Hydrating Face Serum",
+        "altName": [
+            "Moisturizing Serum",
+            "Skin Glow Serum"
+        ],
+        "images": [
+            "https://example.com/images/serum1.jpg",
+            "https://example.com/images/serum2.jpg"
+        ],
+        "price": 3200,
+        "lastPrice": 4500,
+        "stock": 120,
+        "description": "A lightweight hydrating serum infused with hyaluronic acid and vitamin C. Helps to restore skin moisture, reduce dullness, and improve skin elasticity for a radiant glow.",
+        "__v": 0
+    },
+    {
+        "_id": "68b5b1832210948fc15df000",
+        "productId": "B2002",
+        "productName": "Laptop Dell Inspiron 15",
+        "altName": [
+            "Dell 15",
+            "Inspiron Laptop"
+        ],
+        "images": [
+            "https://example.com/images/laptop-front.jpg",
+            "https://example.com/images/laptop-back.jpg"
+        ],
+        "price": 145000,
+        "lastPrice": 150000,
+        "stock": 25,
+        "description": "Dell Inspiron 15 laptop with Intel i5 processor, 8GB RAM, 512GB SSD.",
+        "__v": 0
+    }
+])
+    useEffect(
+        ()=>{
+            axios.get("http://localhost:5000/api/product").then(
+                (res)=>{
+                console.log(res.data)
+                setProducts(res.data)
+                }
+            )
+        },[]
+    )
+    
 
     return(
-        <div>
-            <h1>Admin Product Page</h1>
+        <div>           
+            {
+                products.map(
+                    (product)=>{
+                        return(
+                            <div>
+                                {product.productName}
+                            </div>
+                        )
+                    }
+                )
+            }
         </div>
     )
 }
