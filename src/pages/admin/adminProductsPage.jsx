5cc740cc87ee9a5e6ad1bf56571fc745015bbcb8
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-09-11 13:31:20 +0530

    install supabase library and delete testing.css file💥

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,24 +1,30 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import toast from "react-hot-toast"
 import { FaPlus, FaTrash } from "react-icons/fa"
 import { FaPencil } from "react-icons/fa6"
 import { Link } from "react-router-dom"
 
 export default function AdminProduct() {
   const [products, setProducts] = useState([])
+  const [productLoaded, setProductLoaded] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/product").then((res) => {
-      console.log(res.data)
-      setProducts(res.data)
-    })
-  }, [])
+    if(!productLoaded){
+            axios.get("http://localhost:5000/api/product").then((res) => {
+            setProducts(res.data)
+            setProductLoaded(true)
+        })
+    }
+    
+  }, [productLoaded]);
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-4 text-gray-800 ">Admin Products</h1>
 
-      <div className="overflow-x-auto rounded-2xl shadow-lg border border-gray-200 ">
+      {
+        productLoaded?<div className="overflow-x-auto rounded-2xl shadow-lg border border-gray-200 ">
         <Link to={"/admin/products/addProduct"} className="absolute right-[25px] bottom-[25px] text-[25px] text-white bg-[#3d82f6] p-4 rounded-xl hover:bg-blue-300 "><FaPlus/></Link>
         <table className="min-w-full text-left text-sm text-gray-700">
           <thead className="bg-gray-100 text-gray-800 text-sm uppercase tracking-wide">
@@ -51,7 +57,18 @@ export default function AdminProduct() {
                   {product.description}
                 </td>
                 <td className="px-6 py-4 flex items-center justify-center gap-3">
-                  <button className="p-2 rounded-full bg-red-100 text-red-600 hover:bg-red-200 transition">
+                  <button className="p-2 rounded-full bg-red-100 text-red-600 hover:bg-red-200 transition"
+                   onClick={()=>{
+                    const token = localStorage.getItem("token");
+                    axios.delete(`http://localhost:5000/api/product/${product.productId}`,{
+                        headers: {
+                             Authorization: `Bearer ${token}`
+                        },
+                    }).then(()=>{
+                        toast.success("Product Deleted Successfully");
+                    })
+                    setProductLoaded(false);
+                  }}>
                     <FaTrash />
                   </button>
                   <button className="p-2 rounded-full bg-blue-100 text-blue-600 hover:bg-blue-200 transition">
@@ -62,7 +79,12 @@ export default function AdminProduct() {
             ))}
           </tbody>
         </table>
+      </div>:<div className="w-full h-full flex justify-center items-center">
+        <div className="w-[60px] h-[60px] border-[4px] border-gray-200 border-b-[#3d82f6] rounded-full animate-spin"></div>
       </div>
+      }
+
+      
     </div>
   )
 }
