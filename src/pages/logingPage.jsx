a73379e20bbab1f1f65c7392dfb81d57f91d7fc7
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-10-25 10:04:13 +0530

    redesign login page😊👍

--- src/pages/logingPage.jsx
+++ src/pages/logingPage.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
 
 export default function LoginPage() {
-  const [email,setEmail] = useState("Your email")
+  const [email,setEmail] = useState()
   const [password,setPassword] = useState()
   function login(){
     axios.post(import.meta.env.VITE_BACKEND_URL+"/api/users/login",{
@@ -29,26 +30,38 @@ export default function LoginPage() {
     )
   }
   return (
-    <div className="w-full h-screen bg-red-400 flex justify-center items-center">
-      <div className="flex justify-center items-center flex-col w-[450px] h-[450px] bg-blue-400">
-        <img src="/logo.jpg" className="rounded-full w-[100px] h-[100px]"/>
-        <span>Email</span>
-
-
-        <input className="bg-white" defaultValue={email} onChange={
-            (e)=>{
-              setEmail(e.target.value)
-            }
-          }/>
-        <span>Password</span>
-        <input type="password" className="bg-white" defaultValue={password} onChange={
-          (e)=>{
-            setPassword(e.target.value)
-          }
-        }/>
-
-
-        <button onClick={login} className="bg-white">Login</button>
+    <div className="w-full h-screen bg-gradient-to-r from-blue-400 to-teal-400 flex justify-center items-center">
+      <div className="lg:w-1/3 2xl:w-1/4 p-10 bg-white">
+        <div className="space-y-3">
+          <h1 className="text-center text-3xl font-bold py-5">Welcome To <span className="text-blue-500">HGCK</span></h1>
+          <h1 className="text-xl font-semibold">Sign In</h1>
+          <form className="flex flex-col space-y-3">
+            <input className="outline-0 px-5 py-5 bg-gray-100" placeholder="Enter Your Email" defaultValue={email} onChange={
+                (e)=>{
+                  setEmail(e.target.value)
+                }
+              }/>
+            <input type="password" className="outline-0 px-5 py-5 bg-gray-100" placeholder="Enter Your Password" defaultValue={password} onChange={
+              (e)=>{
+                setPassword(e.target.value)
+              }
+            }/>
+            <div className="flex justify-center items-center">
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" name=""/>
+                <p className="text-sm">Remember Me</p>
+              </div>
+            </div>
+            <button onClick={login} className="p-2 bg-blue-500 text-xl text-white font-bold rounded-md drop-shado-md hover:bg-blue-400">Login</button>
+            <p className="text-center text-sm pb-5 border-b"><span className="font-bold">Terms&Conditions</span> and <span className="font-bold">Privacy</span></p>
+          </form>
+          <div className="space-y-3">
+            <p className="text-center">OR SINGN IN</p>
+            <button className="w-full p-4 bg-white border drop-shadow-md rounded-md space-x-5 flex items-center justify-center">
+              <FcGoogle className="text-2xl" /><span>Sign In With Google</span>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
